22de2948a06f8a8b47aab0e0ae0ee718cf66fb92
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-03-31 23:21:17 -0700

    improved code readability

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,9 @@ import os
 import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
+
 date_data = []
 financial_data = []
-profit_change = []
 
 with open(csvpath) as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=',')
@@ -18,47 +18,53 @@ with open(csvpath) as csvfile:
 		financial_data.append(int(row[1]))
 
 # The total number of months included in the dataset
+
 total_months = len(date_data)
 
-print(f"Total Months: {total_months}")
-
 # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-total = sum(financial_data)
 
-print(f"Total: ${total}")
+total = sum(financial_data)
 
 ind_changes = [financial_data[i+1]-financial_data[i] for i in range(total_months-1)]
 
 average_change = round((sum(ind_changes)/len(ind_changes)),2)
 
-print(f"Average Change: ${average_change}")
-
 # The greatest increase in profits (date and amount) over the entire period
+
 greatest_increase = max(ind_changes)
+
 index_greatest = ind_changes.index(greatest_increase)
 
 greatest_month = date_data[index_greatest+1]
 
-print(f"Greatest Increase in Profits: {greatest_month} (${greatest_increase})")
-
 # The greatest decrease in losses (date and amount) over the entire period
+
 greatest_decrease = min(ind_changes)
+
 index_least = ind_changes.index(greatest_decrease)
 
 least_month = date_data[index_least+1]
 
-print(f"Greatest Decrease in Profits: {least_month} (${greatest_decrease})")
+# Final Analysis
+
+months = (f"Total Months: {total_months} \n")
+profit = (f"Total: ${total} \n")
+profit_change = (f"Average Change: ${average_change} \n")
+profit_increase = (f"Greatest Increase in Profits: {greatest_month} (${greatest_increase}) \n")
+profit_decrease = (f"Greatest Decrease in Profits: {least_month} (${greatest_decrease}) \n")
+Lines = [months, profit, profit_change, profit_increase, profit_decrease]
+
+# Exporting text file containing final analysis
 
 output_path = os.path.join('Analysis', 'Financial_Analysis.txt')
 output_file = open(output_path, 'w')
 
-output_file.write('Financial Analysis')
+output_file.write('Financial Analysis \n')
+output_file.writelines(Lines)
+output_file.close()
 
+# Printing final analysis to terminal
 
-#  Financial Analysis
-#  ----------------------------
-#  Total Months: 86
-#  Total: $38382578
-#  Average  Change: $-2315.12
-#  Greatest Increase in Profits: Feb-2012 ($1926159)
-#  Greatest Decrease in Profits: Sep-2013 ($-2196167)
+output_file = open(output_path, 'r')
+print(output_file.read())
+output_file.close()
