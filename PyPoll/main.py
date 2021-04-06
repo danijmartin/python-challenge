55dc23b3d48d63b811d12b76f20395885096a532
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-04-05 18:01:10 -0700

    Added analysis summary and output file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,17 +16,18 @@ with open(csvpath) as csvfile:
 # Calculate the total number of votes cast
 
 total_votes = len(voter_id)
-print(total_votes)
+
+# print(total_votes)  - tested output
 
 # Calculate a complete list of candidates who received votes
 
 unique_candidates = list(set(candidates))
-print(unique_candidates)
+
+# print(unique_candidates)  - tested output
 
 # Calculate the total number of votes each candidate won
 # Calculate the percentage of votes each candidate won
 
-c_stats = []
 candidates_stats = []
 
 for c in unique_candidates:
@@ -35,21 +36,41 @@ for c in unique_candidates:
   c_stats = [c, vote_percent, c_count]
   candidates_stats.append(c_stats)
 
-print(candidates_stats)
+candidates_stats.sort(key=lambda x:x[1], reverse=True )
 
+# print(candidates_stats) - tested output
 
+# Preparing Candidate totals for printing:
+
+Lines = []
+
+for lst in candidates_stats:
+    line = (f'\n {lst[0]}: {lst[1]}% ({lst[2]})')
+    Lines.append(line)
+
+# print(Lines) - tested output
 
 # Calculate the winner of the election based on popular vote.
 
+winner = candidates_stats[0][0]
 
-  # Election Results
-  # -------------------------
-  # Total Votes: 3521001
-  # -------------------------
-  # Khan: 63.000% (2218231)
-  # Correy: 20.000% (704200)
-  # Li: 14.000% (492940)
-  # O'Tooley: 3.000% (105630)
-  # -------------------------
-  # Winner: Khan
-  # -------------------------
+# print(winner) - tested output
+
+# Exporting text file containing vote analysis
+
+output_path = os.path.join('Analysis', 'Election_Results.txt')
+
+with open(output_path, 'w') as output_file:
+  output_file.write(' Election Results \n')
+  output_file.write('-'*25)
+  output_file.write(f'\n Total Votes: {total_votes} \n')
+  output_file.write('-'*25)
+  output_file.writelines(Lines)
+  output_file.write('\n' + '-'*25)
+  output_file.write(f'\n Winner: {winner} \n')
+  output_file.write('-'*25)
+
+# Printing final analysis to terminal
+
+with open(output_path, 'r') as output_file:
+  print(output_file.read())
